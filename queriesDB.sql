be38c24db3d0265214b0894d091030522d7e2363
Piotr Kaźmierski <217847@edu.p.lodz.pl>
2020-01-10 09:43:05 +0100

    Naprawa funkcji yet_to_pay oraz View not_paid_today.

--- queriesDB.sql
+++ queriesDB.sql
@@ -1,162 +1,110 @@
 -- QUERIES
 
 -- 1. Kto ma najwięcej oplaconych rezerwacji
-SELECT
-    COUNT(g.id),
-    g.first_name,
-    g.last_name
-FROM
-    guest g
-    LEFT JOIN guests_in_reservation pfr
-    ON g.id = pfr.guest_id
-    LEFT JOIN reservation r
-    ON r.id = pfr.reservation_id
-GROUP BY
-    g.id,
-    g.first_name,
-    g.last_name;
+SELECT COUNT(g.id), g.first_name, g.last_name
+FROM guest g
+LEFT JOIN guests_in_reservation pfr
+ON g.id = pfr.guest_id
+LEFT JOIN reservation r
+ON r.id = pfr.reservation_id
+GROUP BY g.id, g.first_name, g.last_name;
 
 -- 2. Jaki klient zaplacil w najwiekszej liczbie rat platności
 
-SELECT
-    g.first_name,
-    g.last_name
-FROM
-    guest g
-    LEFT JOIN guests_in_reservation gir
-    ON g.id = gir.reservation_id
-    LEFT JOIN reservation res
-    ON res.id = gir.reservation_id
-    LEFT JOIN payments_for_reservation pfr
-    ON pfr.reservation_id = res.id
-GROUP BY
-    g.first_name,
-    g.last_name;
+SELECT g.first_name, g.last_name
+FROM guest g
+LEFT JOIN guests_in_reservation gir
+ON g.id = gir.reservation_id
+LEFT JOIN reservation res
+ON res.id = gir.reservation_id
+LEFT JOIN payments_for_reservation pfr
+ON pfr.reservation_id = res.id
+GROUP BY g.first_name, g.last_name;
 
 -- 3. Największa suma rezerwacji/najdroższa rezerwacja
 
-SELECT
-    res.id,
-    SUM(rt.base_price)
-FROM
-    room rom
-    LEFT JOIN room_type rt
-    ON rom.room_type_id = rt.id
-    INNER JOIN reservation res
-    ON rom.id = res.room_id
-GROUP BY
-    res.id
-ORDER BY
-    SUM(rt.base_price) DESC
+SELECT res.id, SUM(rt.base_price)
+FROM room rom
+LEFT JOIN room_type rt
+ON rom.room_type_id = rt.id
+INNER JOIN reservation res
+ON rom.id = res.room_id
+GROUP BY res.id
+ORDER BY SUM(rt.base_price) DESC
 OFFSET 0 ROWS FETCH NEXT 1 ROWS ONLY;
 
 -- 4. Klienci którzy dokonali jednej rezerwacji
 
-SELECT
-    g.first_name,
-    g.last_name
-FROM
-    guest g
-    LEFT JOIN guests_in_reservation gir
-    ON g.id = gir.guest_id
-    LEFT JOIN reservation res
-    ON res.id = gir.reservation_id
-    LEFT JOIN reservation_status rs
-    ON rs.id = res.reservation_status_id
-WHERE
-    rs.name = 'completed';
+SELECT g.first_name, g.last_name
+FROM guest g
+LEFT JOIN guests_in_reservation gir
+ON g.id = gir.guest_id
+LEFT JOIN reservation res
+ON res.id = gir.reservation_id
+LEFT JOIN reservation_status rs
+ON rs.id = res.reservation_status_id
+WHERE rs.name = 'completed';
 
 -- 5. Liczba pokoi ze względu na piętro
 
-SELECT
-    "Floor",
-    COUNT("Floor") "Number of rooms"
-FROM
-    (
-        SELECT
-            substr(id, 0, 1) "Floor"
-        FROM
-            room
-    )
-GROUP BY
-    "Floor";
+SELECT "Floor", COUNT("Floor") "Number of rooms"
+FROM (
+    SELECT substr(id, 0, 1) "Floor"
+    FROM room
+)
+GROUP BY "Floor";
 
 -- 6. Ile jednej nocy można maksymalnie przenocować gości
 
-SELECT
-    SUM(max_tenants)
-FROM
-    room;
+SELECT SUM(max_tenants)
+FROM room;
 
 -- 7. Ile jest rezerwacji dzisiejszego dnia
 
-SELECT
-    COUNT(*)
-FROM
-    reservation r
-WHERE
-    to_char(sysdate, 'yyyy/mm/dd') BETWEEN r.checkin_date AND r.checkout_date;
+SELECT COUNT(*)
+FROM reservation r
+WHERE to_char(sysdate, 'yyyy/mm/dd') BETWEEN r.checkin_date AND r.checkout_date;
 
 -- 8. Najpopularnieszy typ platnosci
 
-SELECT
-    pt.name,
-    COUNT(p.payment_type_id)
-FROM
-    payment p
-    LEFT JOIN payment_type pt
-    ON p.payment_type_id = pt.id
-GROUP BY
-    pt.name
-ORDER BY
-    COUNT(p.payment_type_id) DESC
+SELECT pt.name, COUNT(p.payment_type_id)
+FROM payment p
+LEFT JOIN payment_type pt
+ON p.payment_type_id = pt.id
+GROUP BY pt.name
+ORDER BY COUNT(p.payment_type_id) DESC
 FETCH FIRST 1 ROWS ONLY;
 
 -- 9. Najczęściej rezerwowany pokój
 
-SELECT
-    r.id,
-    COUNT(gir.reservation_id)
-FROM
-    guests_in_reservation gir
-    LEFT JOIN reservation res
-    ON gir.reservation_id = res.id
-    LEFT JOIN room r
-    ON r.id = res.room_id
-GROUP BY
-    r.id
-ORDER BY
-    COUNT(gir.reservation_id)
+SELECT r.id, COUNT(gir.reservation_id)
+FROM guests_in_reservation gir
+LEFT JOIN reservation res
+ON gir.reservation_id = res.id
+LEFT JOIN room r
+ON r.id = res.room_id
+GROUP BY r.id
+ORDER BY COUNT(gir.reservation_id)
 FETCH FIRST 1 ROWS ONLY;
 
 
 -- 10. Pokoje zarezerwowane przez zlotych klientow
 
-SELECT
-    g.first_name,
-    g.last_name,
-    res.room_id
-FROM
-    guest g
-    LEFT JOIN guests_in_reservation gir
-    ON g.id = gir.guest_id
-    LEFT JOIN reservation res
-    ON res.id = gir.reservation_id
-    LEFT JOIN guest_status gs
-    ON gs.id = g.status_id
-WHERE
-    gs.status_name = 'gold' AND res.room_id IS NOT NULL;
+SELECT g.first_name, g.last_name, res.room_id
+FROM guest g
+LEFT JOIN guests_in_reservation gir
+ON g.id = gir.guest_id
+LEFT JOIN reservation res
+ON res.id = gir.reservation_id
+LEFT JOIN guest_status gs
+ON gs.id = g.status_id
+WHERE gs.status_name = 'gold' AND res.room_id IS NOT NULL;
 
 -- 11. Najwyższa i najniższa cena zapłacona dotychczas za każdy pokój
 
-SELECT
-    room_id,
-    MIN(price),
-    MAX(price)
-FROM
-    reservation
-GROUP BY
-    room_id;
+SELECT room_id, MIN(price), MAX(price)
+FROM reservation
+GROUP BY room_id;
 	
     
     
@@ -166,37 +114,15 @@ GROUP BY
 -- dodaj gościa
 
 CREATE OR REPLACE PROCEDURE add_guest (
-    p_first_name guest.first_name%TYPE,
-    p_last_name guest.last_name%TYPE,
-    p_phone guest.phone%TYPE,
-    p_email guest.email%TYPE,
-    p_nationality guest.nationality%TYPE,
-    p_document_id guest.document_id%TYPE,
-    p_birth_date guest.birth_date%TYPE,
-    p_document_type_id guest.document_type_id%TYPE,
-    p_status_id guest.status_id%TYPE
+    p_first_name guest.first_name%TYPE, p_last_name guest.last_name%TYPE, p_phone guest.phone%TYPE, p_email guest.email%TYPE, p_nationality
+    guest.nationality%TYPE, p_document_id guest.document_id%TYPE, p_birth_date guest.birth_date%TYPE, p_document_type_id guest.document_type_id
+    %TYPE, p_status_id guest.status_id%TYPE
 ) AS
 BEGIN
     INSERT INTO guest (
-        first_name,
-        last_name,
-        phone,
-        email,
-        nationality,
-        document_id,
-        birth_date,
-        document_type_id,
-        status_id
+        first_name, last_name, phone, email, nationality, document_id, birth_date, document_type_id, status_id
     ) VALUES (
-        p_first_name,
-        p_last_name,
-        p_phone,
-        p_email,
-        p_nationality,
-        p_document_id,
-        p_birth_date,
-        p_document_type_id,
-        p_status_id
+        p_first_name, p_last_name, p_phone, p_email, p_nationality, p_document_id, p_birth_date, p_document_type_id, p_status_id
     );
 
 END add_guest;
@@ -205,16 +131,13 @@ END add_guest;
 -- dodaj płatność
 
 CREATE OR REPLACE PROCEDURE add_payment (
-    p_amount payment.amount%TYPE,
-    p_payment_type_id payment.payment_type_id%TYPE
+    p_amount payment.amount%TYPE, p_payment_type_id payment.payment_type_id%TYPE
 ) AS
 BEGIN
     INSERT INTO payment (
-        amount,
-        payment_type_id
+        amount, payment_type_id
     ) VALUES (
-        p_amount,
-        p_payment_type_id
+        p_amount, p_payment_type_id
     );
 
 END;
@@ -228,8 +151,7 @@ BEGIN
     UPDATE reservation
     SET
         reservation_status_id = 4
-    WHERE
-        id = p_reservation_id;
+    WHERE id = p_reservation_id;
 
 END;
 
@@ -245,15 +167,12 @@ CREATE OR REPLACE FUNCTION is_paid (
 ) RETURN BOOLEAN AS
     v_reservation_status_name reservation_status.name%TYPE;
 BEGIN
-    SELECT
-        rs.name
+    SELECT rs.name
     INTO v_reservation_status_name
-    FROM
-        reservation r
-        JOIN reservation_status rs
-        ON r.reservation_status_id = rs.id
-    WHERE
-        r.id = p_reservation_id;
+    FROM reservation r
+    JOIN reservation_status rs
+    ON r.reservation_status_id = rs.id
+    WHERE r.id = p_reservation_id;
 
     IF v_reservation_status_name = 'confirmed' THEN
         RETURN true;
@@ -265,8 +184,7 @@ END;
 -- DO POPRAWY cena za pokoj
 
 CREATE OR REPLACE FUNCTION room_price (
-    p_guest_id guest.id%TYPE,
-    p_room_id room.id%TYPE
+    p_guest_id guest.id%TYPE, p_room_id room.id%TYPE
 ) RETURN NUMBER AS
 
     v_calculated_price room_type.base_price%TYPE;
@@ -274,34 +192,25 @@ CREATE OR REPLACE FUNCTION room_price (
     v_season_multiplier season_pricing.multiplier%TYPE;
     v_guest_status_multiplier guest_status.multiplier%TYPE;
 BEGIN
-    SELECT
-        rt.base_price
+    SELECT rt.base_price
     INTO v_base_price
-    FROM
-        room r
-        LEFT JOIN room_type rt
-        ON r.room_type_id = rt.id
-    WHERE
-        r.id = p_room_id;
+    FROM room r
+    LEFT JOIN room_type rt
+    ON r.room_type_id = rt.id
+    WHERE r.id = p_room_id;
 
-    SELECT
-        gs.multiplier
+    SELECT gs.multiplier
     INTO v_guest_status_multiplier
-    FROM
-        guest g
-        LEFT JOIN guest_status gs
-        ON g.status_id = gs.id
-    WHERE
-        g.id = p_guest_id;
+    FROM guest g
+    LEFT JOIN guest_status gs
+    ON g.status_id = gs.id
+    WHERE g.id = p_guest_id;
 
     BEGIN
-        SELECT
-            multiplier
+        SELECT multiplier
         INTO v_season_multiplier
-        FROM
-            season_pricing
-        WHERE
-            sysdate >= start_date AND sysdate <= end_date;
+        FROM season_pricing
+        WHERE sysdate >= start_date AND sysdate <= end_date;
 
     EXCEPTION
         WHEN no_data_found THEN
@@ -318,19 +227,14 @@ CREATE OR REPLACE FUNCTION season_for_reservation (
 ) RETURN season_pricing.id%TYPE AS
     v_reservation_date reservation.checkin_date%TYPE;
     CURSOR season_cursor IS
-    SELECT
-        *
-    FROM
-        season_pricing;
+    SELECT *
+    FROM season_pricing;
 
 BEGIN
-    SELECT
-        r.checkin_date
+    SELECT r.checkin_date
     INTO v_reservation_date
-    FROM
-        reservation r
-    WHERE
-        r.id = p_reservation_id;
+    FROM reservation r
+    WHERE r.id = p_reservation_id;
 
     FOR season IN season_cursor LOOP IF v_reservation_date BETWEEN season.start_date AND season.end_date THEN
         RETURN season.id;
@@ -343,54 +247,52 @@ END;
 -- DO SPRAWDZENIA Zwracanie całych rzędów pokoi, które są wolne i mają podane parametry (filtrowanie, niech będą defaulty albo jakieś inne ogarnięcie przypadków, gdy nie ma podanego danego parametru)
 
 CREATE OR REPLACE FUNCTION available_rooms (
-    p_date_from reservation.checkin_date%TYPE DEFAULT to_char(sysdate),
-    p_date_to reservation.checkout_date%TYPE DEFAULT to_char(sysdate + 7),
-    p_room_type room_type.name%TYPE DEFAULT '%'
+    p_date_from reservation.checkin_date%TYPE DEFAULT to_char(sysdate), p_date_to reservation.checkout_date%TYPE DEFAULT to_char(
+    sysdate + 7), p_room_type room_type.name%TYPE DEFAULT '%'
 ) RETURN SYS_REFCURSOR AS
     my_cursor SYS_REFCURSOR;
 BEGIN
-    OPEN my_cursor FOR SELECT
-                           r.id,
-                           rt.name,
-                           rt.base_price
-                       FROM
-                           reservation res
-                           LEFT JOIN room r
-                           ON res.room_id = r.id
-                           LEFT JOIN room_type rt
-                           ON rt.id = r.room_type_id
-                       WHERE
-                           ( p_date_from NOT BETWEEN res.checkin_date AND res.checkout_date ) AND ( p_date_to NOT BETWEEN res.checkin_date
-                           AND res.checkout_date ) AND rt.name LIKE p_room_type;
+    OPEN my_cursor FOR SELECT r.id, rt.name, rt.base_price
+                       FROM reservation res
+                       LEFT JOIN room r
+                       ON res.room_id = r.id
+                       LEFT JOIN room_type rt
+                       ON rt.id = r.room_type_id
+                       WHERE ( p_date_from NOT BETWEEN res.checkin_date AND res.checkout_date ) AND ( p_date_to NOT BETWEEN res.checkin_date
+                       AND res.checkout_date ) AND rt.name LIKE p_room_type;
 
     RETURN my_cursor;
 END;
 
 
 
--- DO SPRAWDZENIA Ile z wymaganej kwoty rezerwacji zostało już wpłacone/ile do zapłacenia
+-- OK Ile z wymaganej kwoty rezerwacji zostało już wpłacone/ile do zapłacenia
 
 CREATE OR REPLACE FUNCTION yet_to_pay (
     p_reservation_id reservation.id%TYPE
 ) RETURN NUMBER AS
     v_res_price reservation.price%TYPE;
-    v_payments_sum payment.amount%TYPE;
+    v_payments_sum payment.amount%TYPE := 0;
 BEGIN
-    SELECT
-        p.amount
-    INTO v_payments_sum
-    FROM
-        payment
-    WHERE
-        id = p_reservation_id;
+    BEGIN
+        SELECT SUM(amount)
+        INTO v_payments_sum
+        FROM payment
+        WHERE reservation_id = p_reservation_id;
 
-    SELECT
-        price
+        IF v_payments_sum IS NULL THEN
+            v_payments_sum := 0;
+        END IF;
+    EXCEPTION
+        WHEN no_data_found THEN
+            v_payments_sum := 0;
+    END;
+
+    dbms_output.put_line('res id: ' || p_reservation_id || ', v_payments_sum: ' || v_payments_sum);
+    SELECT price
     INTO v_res_price
-    FROM
-        reservation res
-    WHERE
-        id = p_reservation_id;
+    FROM reservation res
+    WHERE id = p_reservation_id;
 
     RETURN trunc(v_res_price - v_payments_sum, 2);
 END;
@@ -439,13 +341,10 @@ DECLARE
     v_counter NUMBER;
     e_no_season_found EXCEPTION;
 BEGIN
-    SELECT
-        COUNT(*)
+    SELECT COUNT(*)
     INTO v_counter
-    FROM
-        season_pricing sp
-    WHERE
-        :new.start_date >= sp.start_date AND :new.end_date <= sp.end_date;
+    FROM season_pricing sp
+    WHERE :new.start_date >= sp.start_date AND :new.end_date <= sp.end_date;
 
     IF v_counter >= 1 THEN
         RAISE e_no_season_found;
@@ -459,40 +358,32 @@ END;
 -- DO SPRAWDZENIA widok Wolne pokoje (aktualnie)
 
 CREATE OR REPLACE VIEW free_rooms AS
-    SELECT
-        r.id,
-        rt.name,
-        rt.base_price
-    FROM
-        reservation res
-        LEFT JOIN room r
-        ON res.room_id = r.id
-        LEFT JOIN room_type rt
-        ON rt.id = r.room_type_id
-    WHERE
-        SYSDATE NOT BETWEEN res.checkin_date AND res.checkout_date;
+    SELECT r.id, rt.name, rt.base_price
+    FROM reservation res
+    LEFT JOIN room r
+    ON res.room_id = r.id
+    LEFT JOIN room_type rt
+    ON rt.id = r.room_type_id
+    WHERE sysdate NOT BETWEEN res.checkin_date AND res.checkout_date;
 
 
 
--- DO POPRAWY, ten sam problem co poprzedni, czyli p.amount zwróci dla POJEDYNCZEJ platnosci | kto w danym dniu nie ma zapłaty za hotel
+-- OK kto w danym dniu nie ma zapłaty za hotel
 
 CREATE OR REPLACE VIEW not_paid_today AS
-    SELECT
-        g.first_name,
-        g.last_name,
-        res.price - p.amount AS "Left to pay"
-    FROM
-        reservation res
-        LEFT JOIN reservation_status rs
-        ON res.reservation_status_id = rs.id
-        LEFT JOIN guests_in_reservation gir
-        ON gir.reservation_id = res.id
-        LEFT JOIN guest g
-        ON g.id = gir.guest_id
-        LEFT JOIN payment p
-        ON res.id = p.reservation_id
-    WHERE
-        res.checkin_date >= to_char(sysdate) AND res.checkout_date <= to_char(sysdate) AND rs.name = 'unpaid';
+    SELECT res.id, g.first_name, g.last_name, yet_to_pay(res.id) AS "Left to pay"
+    FROM reservation res
+    LEFT JOIN reservation_status rs
+    ON res.reservation_status_id = rs.id
+    LEFT JOIN guests_in_reservation gir
+    ON gir.reservation_id = res.id
+    LEFT JOIN guest g
+    ON g.id = gir.guest_id
+    LEFT JOIN payment p
+    ON res.id = p.reservation_id
+    WHERE rs.name IN (
+        'unpaid', 'partially_paid'
+    );
 
 
 --Testy
